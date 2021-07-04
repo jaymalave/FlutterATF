03594307ee75b5f22e688ecac4e0294ef6aad7cd
Jay Malave <jaym.github@gmail.com>
2021-07-04 20:02:35 +0530

    added gesture detectors

--- lib/home.dart
+++ lib/home.dart
@@ -31,64 +31,81 @@ class Home extends StatelessWidget {
             Container(
               height: 150,
               width: 400,
-              child: new Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                shadowColor: Colors.blueGrey,
-                elevation: 5,
-                color: Colors.white,
-                borderOnForeground: false,
-                child: Column(
-                  children: <Widget>[
-                    const ListTile(
-                      leading: Image(
-                        image: AssetImage('android/assets/mango.jpg'),
-                        height: 200,
-                        width: 100,
+              child: GestureDetector(
+                onLongPress: ()  => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0),),
+          title: const Text('Mango Mafia'),
+          content: const Text('Here goes the description!'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+
+            child: new Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  shadowColor: Colors.blueGrey,
+                  elevation: 5,
+                  color: Colors.white,
+                  borderOnForeground: false,
+                  child: Column(
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Image(
+                          image: AssetImage('android/assets/mango.jpg'),
+                          height: 200,
+                          width: 100,
+                        ),
+                        title:
+                            Text('Mango Mafia', style: TextStyle(fontSize: 25)),
                       ),
-                      title:
-                          Text('Mango Mafia', style: TextStyle(fontSize: 25)),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 30),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "\$2.99",
-                          style: TextStyle(fontSize: 20),
+                      Padding(
+                        padding: EdgeInsets.only(right: 30),
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            "\$2.99",
+                            style: TextStyle(fontSize: 20),
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 5),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 5, left: 50),
-                      child: new Row(
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(left: 5, right: 25),
-                            child: new FloatingActionButton.extended(
-                              heroTag: "mangobtn1",
-                              label: Text("Order Now"),
-                              onPressed: null,
-                              backgroundColor: Colors.amber,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 25),
-                            child: new FloatingActionButton.extended(
-                              heroTag: "mangobtn2",
-                              label: Text("Add to Cart"),
-                              onPressed: null,
-                              backgroundColor: Colors.amber,
-                            ),
-                          ),
-                        ],
+                      Padding(
+                        padding: EdgeInsets.only(top: 5),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: EdgeInsets.only(top: 5, left: 50),
+                        child: new Row(
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(left: 5, right: 25),
+                              child: new FloatingActionButton.extended(
+                                heroTag: "mangobtn1",
+                                label: Text("Order Now"),
+                                onPressed: null,
+                                backgroundColor: Colors.amber,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 25),
+                              child: new FloatingActionButton.extended(
+                                heroTag: "mangobtn2",
+                                label: Text("Add to Cart"),
+                                onPressed: null,
+                                backgroundColor: Colors.amber,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -98,64 +115,80 @@ class Home extends StatelessWidget {
             Container(
               height: 162,
               width: 400,
-              child: new Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                shadowColor: Colors.blueGrey,
-                elevation: 5,
-                color: Colors.white,
-                borderOnForeground: false,
-                child: Column(
-                  children: <Widget>[
-                    const ListTile(
-                      leading: Image(
-                        image: AssetImage('android/assets/mango.jpg'),
-                        height: 200,
-                        width: 100,
+              child: GestureDetector(
+                                onLongPress: ()  => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0),),
+          title: const Text('Apple Afficiado'),
+          content: const Text('Here goes the description!'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+                child: new Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  shadowColor: Colors.blueGrey,
+                  elevation: 5,
+                  color: Colors.white,
+                  borderOnForeground: false,
+                  child: Column(
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Image(
+                          image: AssetImage('android/assets/mango.jpg'),
+                          height: 200,
+                          width: 100,
+                        ),
+                        title: Text('Apple Afficiado',
+                            style: TextStyle(fontSize: 25)),
                       ),
-                      title: Text('Apple Afficiado',
-                          style: TextStyle(fontSize: 25)),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 30),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "\$2.99",
-                          style: TextStyle(fontSize: 20),
+                      Padding(
+                        padding: EdgeInsets.only(right: 30),
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            "\$2.99",
+                            style: TextStyle(fontSize: 20),
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 5, left: 50),
-                      child: new Row(
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(left: 5, right: 25),
-                            child: new FloatingActionButton.extended(
-                              heroTag: "applebtn1",
-                              label: Text("Order Now"),
-                              onPressed: null,
-                              backgroundColor: Colors.amber,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 25),
-                            child: new FloatingActionButton.extended(
-                              heroTag: "applebtn2",
-                              label: Text("Add to Cart"),
-                              onPressed: null,
-                              backgroundColor: Colors.amber,
-                            ),
-                          ),
-                        ],
+                      Padding(
+                        padding: EdgeInsets.only(top: 20),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: EdgeInsets.only(top: 5, left: 50),
+                        child: new Row(
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(left: 5, right: 25),
+                              child: new FloatingActionButton.extended(
+                                heroTag: "applebtn1",
+                                label: Text("Order Now"),
+                                onPressed: null,
+                                backgroundColor: Colors.amber,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 25),
+                              child: new FloatingActionButton.extended(
+                                heroTag: "applebtn2",
+                                label: Text("Add to Cart"),
+                                onPressed: null,
+                                backgroundColor: Colors.amber,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -165,64 +198,80 @@ class Home extends StatelessWidget {
             Container(
               height: 162,
               width: 400,
-              child: new Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0),
-                ),
-                shadowColor: Colors.blueGrey,
-                elevation: 5,
-                color: Colors.white,
-                borderOnForeground: false,
-                child: Column(
-                  children: <Widget>[
-                    const ListTile(
-                      leading: Image(
-                        image: AssetImage('android/assets/mango.jpg'),
-                        height: 200,
-                        width: 100,
+              child: GestureDetector(
+                                onLongPress: ()  => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0),),
+          title: const Text('Orange Outta Hills'),
+          content: const Text('Here goes the description!'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+                child: new Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  shadowColor: Colors.blueGrey,
+                  elevation: 5,
+                  color: Colors.white,
+                  borderOnForeground: false,
+                  child: Column(
+                    children: <Widget>[
+                      const ListTile(
+                        leading: Image(
+                          image: AssetImage('android/assets/mango.jpg'),
+                          height: 200,
+                          width: 100,
+                        ),
+                        title: Text('Orange Outta Hills',
+                            style: TextStyle(fontSize: 25)),
                       ),
-                      title: Text('Orange Outta Hills',
-                          style: TextStyle(fontSize: 25)),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 30),
-                      child: Align(
-                        alignment: Alignment.centerRight,
-                        child: Text(
-                          "\$2.99",
-                          style: TextStyle(fontSize: 20),
+                      Padding(
+                        padding: EdgeInsets.only(right: 30),
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Text(
+                            "\$2.99",
+                            style: TextStyle(fontSize: 20),
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 20),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 5, left: 50),
-                      child: new Row(
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(left: 5, right: 25),
-                            child: new FloatingActionButton.extended(
-                              heroTag: "orangebtn1",
-                              label: Text("Order Now"),
-                              onPressed: null,
-                              backgroundColor: Colors.amber,
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 25),
-                            child: new FloatingActionButton.extended(
-                              heroTag: "orangebtn2",
-                              label: Text("Add to Cart"),
-                              onPressed: null,
-                              backgroundColor: Colors.amber,
-                            ),
-                          ),
-                        ],
+                      Padding(
+                        padding: EdgeInsets.only(top: 20),
                       ),
-                    ),
-                  ],
+                      Padding(
+                        padding: EdgeInsets.only(top: 5, left: 50),
+                        child: new Row(
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(left: 5, right: 25),
+                              child: new FloatingActionButton.extended(
+                                heroTag: "orangebtn1",
+                                label: Text("Order Now"),
+                                onPressed: null,
+                                backgroundColor: Colors.amber,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 25),
+                              child: new FloatingActionButton.extended(
+                                heroTag: "orangebtn2",
+                                label: Text("Add to Cart"),
+                                onPressed: null,
+                                backgroundColor: Colors.amber,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
